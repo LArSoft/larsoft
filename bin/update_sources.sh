ff8db40c8925a4c3995181fcfb3d60832fcbfa92
Lynn Garren <garren@fnal.gov>
2016-02-10 20:33:59 -0600

    update the refactor scripts

--- bin/update_sources.sh
+++ bin/update_sources.sh
@@ -87,13 +87,12 @@ if [[ -n "${file}" ]]; then
     one_file "$file"
   fi
 else
-  for F in `find $TOP \( -type d \( -name .git -o -name .svn -o -name CVS -o -name ups -o -path '*/tools/migration' -o -path '*/art/ParameterSet' -o -path '*/paper' \) \
-                       -prune \) -o -type f \! \( -name '*~' -o -name '*.bak' -o -name '*.orig' -o -name '*.new' -o -name '*.gdml' -o -name '*.py' -o -name '*.sh' -o -name '*.o' -o -name '*.so' -o -name '*.os' -o -name '*.root' -o -name '*.txt'  -o -name '*.tex' -o -name 'SConscript' \) -print`; do
+  for F in `find $TOP \( -name "*.c*" -o -name "*.C*" -o -name "*.h*" -o -name "*.H*" \) -print`; do
     one_file "$F"
   done
+  echo
   for F in `find $TOP -name CMakeLists.txt -print`; do
     cmake_file "$F"
-    # now undo DumpTracks_module.cc change
-    sed -i.bak -e's%larreco_TrackFinder_DumpTracks_module.cc%DumpTracks_module.cc%g' $F
   done
+  echo
 fi
