be62b4944c1c60dec1f22aed55dbaaf4da248650
Lynn Garren <garren@fnal.gov>
2017-03-09 16:04:50 -0600

    working script

--- bin/v06_30_00_rc0-art206/fix_cmake_art206.pl
+++ bin/v06_30_00_rc0-art206/fix_cmake_art206.pl
@@ -4,7 +4,8 @@ use vars qw(%dir_list);
 BEGIN { %dir_list = (
 	"canvas_Utilities" => "canvas",
 	"canvas_Persistency_Common" => "canvas",
-	"canvas_Persistency_Provenance" => "canvas"
+	"canvas_Persistency_Provenance" => "canvas",
+        "cetlib" => "cetlib cetlib_except"
                        ); }
 
 foreach my $lib (sort keys %dir_list) {
@@ -12,7 +13,10 @@ foreach my $lib (sort keys %dir_list) {
    next if m&simple_plugin&i;
    next if m&SUBDIRNAME&i;
    next if m&SUBDIRS&i;
+   next if m&cetlib cetlib_except&i;
   #s&\b\Q${lib}\E([^\.\s]*\b)([^\.]|$)&$dir_list{$lib}${1}${2}&g and last;
   s&\b\Q${lib}\E\b([^\.]|$)&$dir_list{$lib}${1}${2}&g and last;
 }
+
+s/\$\{CETLIB\}/cetlib cetlib_except/g;
 
--- bin/v06_30_00_rc0-art206/fix_headers_art206.pl
+++ bin/v06_30_00_rc0-art206/fix_headers_art206.pl
@@ -5,16 +5,7 @@ use vars qw(%header_list);
 
 # explicit headers to avoid conflicts with experiment code
 BEGIN { %header_list = (
-"lardata/RecoBaseArt/Dumpers/DumpAssociations.h" => "lardata/ArtDataHelper/Dumpers/DumpAssociations.h",
-"lardata/RecoBaseArt/Dumpers/NewLine.h" => "lardata/ArtDataHelper/Dumpers/NewLine.h",
-"lardata/RecoBaseArt/Dumpers/PCAxisDumpers.h" => "lardata/ArtDataHelper/Dumpers/PCAxisDumpers.h",
-"lardata/RecoBaseArt/Dumpers/SpacePointDumpers.h" => "lardata/ArtDataHelper/Dumpers/SpacePointDumpers.h",
-"lardata/RecoBaseArt/Dumpers/hexfloat.h" => "lardata/ArtDataHelper/Dumpers/hexfloat.h",
-"lardata/RecoBaseArt/FindAllP.h" => "lardata/ArtDataHelper/FindAllP.h",
-"lardata/RecoBaseArt/HitCreator.h" => "lardata/ArtDataHelper/HitCreator.h",
-"lardata/RecoBaseArt/HitUtils.h" => "lardata/ArtDataHelper/HitUtils.h",
-"lardata/RecoBaseArt/TrackUtils.h" => "lardata/ArtDataHelper/TrackUtils.h",
-"lardata/RecoBaseArt/WireCreator.h" => "lardata/ArtDataHelper/WireCreator.h"
+"cetlib/demangle.h" => "cetlib_except/demangle.h"
 		       ); }
 
 foreach my $inc (sort keys %header_list) {
