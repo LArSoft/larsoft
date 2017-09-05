942e8c153b66542636d84bb4409165b2a72539a9
Lynn Garren <garren@fnal.gov>
2017-09-05 13:54:03 -0500

    add some protections

--- bin/v06_49_00-PtrMakerInArt.sh
+++ bin/v06_49_00-PtrMakerInArt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for pfile in `ack -l PtrMaker`
+for pfile in `ack -l PtrMaker | grep -v base_dependency_database | grep -v PtrMakerInArt.sh | grep -v PtrMaker.h | grep -v CMakeLists.txt`
 do
   sed -i -e s%lardata/Utilities/PtrMaker.h%art/Persistency/Common/PtrMaker.h% $pfile
   sed -i -e s%lar::PtrMaker%art::PtrMaker%g $pfile
