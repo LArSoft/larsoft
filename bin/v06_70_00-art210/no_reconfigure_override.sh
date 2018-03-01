b82b8f53f6506579128936981ba6262a3e2070f1
Lynn Garren <garren@fnal.gov>
2018-03-01 16:58:11 -0600

    make it work

--- bin/v06_70_00-art210/no_reconfigure_override.sh
+++ bin/v06_70_00-art210/no_reconfigure_override.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sed -r "s/(.*void[[:space:]]+reconfigure\(.*\))[[:space:]]*override[[:space:]]*(.*)/\1 \2/g” <input files>
+for pfile in `ack -l "reconfigure(.*)override"`
+do
+  echo "checking ${pfile}"
+  sed -i.bak -r "s%(.*void[[:space:]]+reconfigure\(.*\))[[:space:]]*override[[:space:]]*(.*)%\1 \2%g" ${pfile}
+done
+
+exit 0
