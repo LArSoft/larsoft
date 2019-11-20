3a3661e259f56dc0fe840b155f2cd1c25565e999
Lynn Garren <garren@fnal.gov>
2019-11-20 10:46:33 -0600

    simple script to remove trailing whitespace

---
+++ bin/removeWhiteSpace.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+usage()
+{
+   echo "USAGE: `basename ${0}` <file list>"
+}
+
+file_list=$@
+
+if [ -z ${file_list} ]; then
+  usage
+  exit 1
+fi
+
+for fl in ${file_list}; do
+  #echo "remove trailing whitespace from ${fl}"
+  sed -i 's/[ \t]*$//g' ${fl}
+done
+
+exit 0
+
