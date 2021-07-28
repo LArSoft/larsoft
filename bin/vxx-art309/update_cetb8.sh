f42db65fe443fde7a5789c042d6d5381920ed3c7
Lynn Garren <garren@fnal.gov>
2021-07-28 12:34:11 -0500

    updating library names - incomplete

---
+++ bin/vxx-art309/update_cetb8.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# Program name
+prog=${0##*/}
+# ======================================================================
+function usage() {
+    cat 1>&2 <<EOF
+usage: $prog [--one-file <file>] <top-dir>
+EOF
+}
+
+get_this_dir() 
+{
+    ( cd / ; /bin/pwd -P ) >/dev/null 2>&1
+    if (( $? == 0 )); then
+      pwd_P_arg="-P"
+    fi
+    reldir=`dirname ${0}`
+    thisdir=`cd ${reldir} && /bin/pwd ${pwd_P_arg}`
+}
+
+##function one_file() {
+##  local F=$1
+##  printf "$F \n"
+##  # Optionally fix whitespace
+##  (( ${fix_whitespace:-0} )) && ed "$F" < fix-whitespace.ed > /dev/null 2>&1
+##  # Fix includes 
+##  perl -wapi\~ -f ${thisdir}/fix-header-locs-nutools.pl "${F}" >/dev/null 2>&1 && rm -f "${F}~"
+##}
+
+function cmake_file() {
+  local F=$1
+  printf "$F \n"
+  # Optionally fix whitespace
+  (( ${fix_whitespace:-0} )) && ed "$F" < fix-whitespace.ed > /dev/null 2>&1
+  # Fix CMakeLists.txt 
+  perl -wapi\~ -f ${thisdir}/update_cmake_cetb8.pl "${F}" >/dev/null 2>&1 && rm -f "${F}~"
+}
+
+# ======================================================================
+# Prepare:
+getopt -T >/dev/null 2>&1
+if (( $? != 4 )); then
+  echo "ERROR: GNU getopt required! Check SETUP_GETOPT and PATH." 1>&2
+  exit 1
+fi
+
+TEMP=`getopt -n "$prog" -o a --long all-lumi-cases --long one-file: --long no-fix-pset -- "${@}"`
+eval set -- "$TEMP"
+while true; do
+  case $1 in
+    --fix-whitespace)
+      fix_whitespace=1
+      shift
+      ;;
+    --one-file)
+      file=$2
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Bad argument \"$OPT\"" 1>&2
+      usage
+      exit 1
+    esac
+done
+
+TOP=${1}
+
+get_this_dir
+
+# ======================================================================
+# Run scripts to update
+
+TMP=`mktemp -t update_sources.sh.XXXXXX`
+trap "rm $TMP* 2>/dev/null" EXIT
+
+set -x
+if [[ -n "${file}" ]]; then
+  if ! [[ -r "${file}" ]]; then
+    echo "ERROR: ${file} does not exist or is not readable." 1>&2
+    exit 1
+  else
+    cmake_file "$file"
+  fi
+else
+##  for F in `find $TOP \( -name "*.c*" -o -name "*.C*" -o -name "*.icc" -o -name "*.h*" -o -name "*.H*" \) -print`; do
+##    one_file "$F"
+##  done
+##  echo
+  for F in `find $TOP -name CMakeLists.txt -print`; do
+    cmake_file "$F"
+  done
+  echo
+fi
+set +x
---
+++ bin/vxx-art309/update_cmake_cetb8.pl
@@ -0,0 +1,51 @@
+use strict;
+
+use vars qw(%dir_list);
+BEGIN { %dir_list = (
+        "art_Persistency_Common" => "art::Persistency_Common",
+        "art_Persistency_Provenance" => "art::Persistency_Provenance",
+        "art_Framework_Principal" => "art::Framework_Principal",
+        "art_Framework_Services_Registry" => "art::Framework_Services_Registry",
+        "art_Framework_Services_Optional_RandomNumberGenerator_service" => "art::Framework_Services_Optional_RandomNumberGenerator_service",
+        "art_Framework_IO_ProductMix" => "art::Framework_IO_ProductMix",
+        "art_Utilities" => "art::Utilities",
+        "canvas" => "canvas::canvas",
+        "cetlib" => "cetlib::cetlib",
+        "cetlib_except" => "cetlib_except::cetlib_except"
+		       ); }
+
+foreach my $lib (sort keys %dir_list) {
+   next if m&add_subdirectory&i;
+   next if m&find_ups_product&i;
+   next if m&simple_plugin&i;
+   next if m&SUBDIRNAME&i;
+   next if m&SUBDIRS&i;
+   next if m&LIBRARY_NAME&i;
+   next if m&PACKAGE&i;
+   next if m&cetlib::cetlib&i;
+   next if m&canvas::canvas&i;
+   next if m&cetlib_except::cetlib_except&i;
+  #s&\b\Q${lib}\E([^\.\s]*\b)([^\.]|$)&$dir_list{$lib}${1}${2}&g and last;
+  s&\b\Q${lib}\E\b([^\.]|$)&$dir_list{$lib}${1}${2}&g and last;
+}
+
+##        "" => "",
+#s&\$\{SIMULATIONBASE\}&nusimdata_SimulationBase&g;
+        s&\$\{ART_FRAMEWORK_SERVICES_REGISTRY\}&art::Framework_Services_Registry&;
+        s&\$\{ART_FRAMEWORK_CORE\}&art::Framework_Core&;
+        s&\$\{ART_FRAMEWORK_IO_SOURCES\}&art::Framework_IO_Sources&;
+        s&\$\{ART_PERSISTENCY_COMMON\}&art::Persistency_Common&;
+        s&\$\{ART_ROOT_IO\}&art_root_io::art_root_io&;
+        s&\$\{ART_ROOT_IO_TFILE_SUPPORT\}&art_root_io::tfile_support&;
+        s&\$\{ART_ROOT_IO_TFILESERVICE_SERVICE\}&art_root_io::TFileService_service&;
+        s&\$\{ART_ROOT_IO_ROOTINPUT_SOURCE\}&art_root_io::RootInput_source&;
+        s&\$\{ART_FRAMEWORK_PRINCIPAL\}&art::Framework_Principal&;
+        s&\$\{MF_MESSAGELOGGER\}&messagefacility::MF_MessageLogger&;
+        s&\$\{FHICLCPP\}&fhiclcpp::fhiclcpp&;
+        s&\$\{CETLIB\}&cetlib::cetlib&;
+        s&\$\{BOOST_DATE_TIME\}&Boost::date_time&;
+        s&\$\{BOOST_FILESYSTEM\}&Boost::filesystem&;
+        s&\$\{BOOST_THREAD\}&Boost::thread&;
+        s&\$\{BOOST_REGEX\}&Boost::regex&;
+        s&\$\{ROOT_CORE\}&ROOT::Core&;
+
